5ef6bf3448da3d627d93e6470b56e8dba9728f21
Sarvesh <sarvesh@Sarveshs-MacBook-Pro.local>
2026-04-15 18:39:14 +0530

    github action added

--- jords-clone/next.config.js
+++ jords-clone/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // config options here
+  output: 'export',
+  basePath: '/jords-clone', // 👈 your repo name
 };
 
 module.exports = nextConfig;
